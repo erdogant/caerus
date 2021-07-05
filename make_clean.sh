e1f67263b545c0fb55755f84f358dc7b242b1a0f
erdogant <erdogant@gmail.com>
2021-07-05 17:54:12 +0200

    update cleanign file

--- make_clean.sh
+++ make_clean.sh
@@ -1,9 +1,17 @@
-echo "Cleaning previous builds first.."
+echo "Cleaning repo first.."
 rm -rf dist
 rm -rf build
 rm -rf caerus.egg-info
 rm -rf __pycache__
 rm -rf .pytest_cache
+rm -rf caerus/__pycache__
+rm -rf caerus/utils/__pycache__
 rm -rf .pylint.d
-rm -rf *.tmp
-rm -rf *.zip
+rm -rf caerus/data/*.zip
+rm -rf caerus/data/*.csv
+rm -rf *.js
+rm -rf *.html
+rm -rf *.css
+rm -rf *.dot
+rm -rf *.png
+rm -rf .coverage*
