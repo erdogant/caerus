53967f57117f5eaf398ed197e0ba3553274af73b
erdogant <erdogant@gmail.com>
2022-03-13 17:09:32 +0100

    update url

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
      description="Python package caerus",
      long_description=long_description,
      long_description_content_type="text/markdown",
-     url="https://github.com/erdogant/caerus",
+     url="https://erdogant.github.io/caerus",
 	 download_url = 'https://github.com/erdogant/caerus/archive/'+new_version+'.tar.gz',
      packages=setuptools.find_packages(), # Searches throughout all dirs for files to include
      include_package_data=True, # Must be true to include files depicted in MANIFEST.in
