839a590515ec5e1c685bec2913e6edc7c5365172
erdogant <erdogant@gmail.com>
2022-05-20 19:58:57 +0200

    0.1.8

--- caerus/__init__.py
+++ caerus/__init__.py
@@ -2,7 +2,7 @@ from caerus.caerus import caerus
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 
 # module level doc-string
 __doc__ = """
