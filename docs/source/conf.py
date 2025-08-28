ba5c8589443ce4919659e66325b641f7972cb3a9
erdogant <erdogant@gmail.com>
2025-08-28 16:44:24 +0200

    Remove conflicting docs workflows, keep only gh-pages deployment

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,7 +87,7 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build"]
-
+""
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "sphinx"
